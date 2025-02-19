6d31d39f21cc35b33a54a701adbda5326f8e401d
gemdude46 <>
2025-02-19 00:11:19 +0000

    Remember direction

--- in_game/agents/player/player.gd
+++ in_game/agents/player/player.gd
@@ -6,13 +6,26 @@ extends Node
 @export var speed: float = 0.0625
 @export var adjust: float = 0.25
 
+var facing: Vector2 = Vector2.DOWN
+
 func _physics_process(_delta: float) -> void:
 	var scale: float = agent.scale.x
 	
-	if input.left: agent.move_x(-speed, adjust * scale)
-	elif input.right: agent.move_x(speed, adjust * scale)
-	elif input.up: agent.move_y(-speed, adjust * scale)
-	elif input.down: agent.move_y(speed, adjust * scale)
+	if input.left:
+		agent.move_x(-speed, adjust * scale)
+		facing = Vector2.LEFT
+		
+	elif input.right:
+		agent.move_x(speed, adjust * scale)
+		facing = Vector2.RIGHT
+		
+	elif input.up:
+		agent.move_y(-speed, adjust * scale)
+		facing = Vector2.UP
+		
+	elif input.down:
+		agent.move_y(speed, adjust * scale)
+		facing = Vector2.DOWN
 	
 	if input.shrink and agent.scale.x > speed * 1.5: agent.change_size(-speed)
 	if input.expand: agent.change_size(speed)
