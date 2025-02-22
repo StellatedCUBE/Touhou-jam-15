60e728a3f691ab40fb8f99750136d2b028ea993c
gemdude46 <>
2025-02-22 09:45:16 +0000

    Wandering

--- in_game/agents/agent/enemy.gd
+++ in_game/agents/agent/enemy.gd
@@ -9,12 +9,23 @@ extends Node
 @export var fire_delay: int = 120
 @export var misfortune_required: int = 0
 @export var knockback_speed: float = 0.08
+@export var speed: float = 0.03125
+@export var frequency: float = 0.75
+@export var sight: float = 5
+
+var noise_x: FastNoiseLite = FastNoiseLite.new()
+var noise_y: FastNoiseLite = FastNoiseLite.new()
+var noise_t: float
+var sight_squared: float
+var bonk: int = 0
 
 var iframes: int = 0
 var knockback: Vector2 = Vector2.ZERO
 var reload_time: int = 0
 
 func _ready() -> void:
+	sight_squared = sight * sight
+	reset_noise()
 	%Area.connect("area_entered", hit)
 
 func hit(area: Area2D) -> void:
@@ -41,7 +52,7 @@ func _physics_process(_delta: float) -> void:
 	if health <= 0:
 		agent.queue_free()
 	
-	if knockback.length_squared() > 0.000001:
+	if knockback.length_squared() > 0.00001:
 		if not agent.move_x(knockback.x, 0.01):
 			knockback.x = 0
 		if not agent.move_y(knockback.y, 0.01):
@@ -49,6 +60,19 @@ func _physics_process(_delta: float) -> void:
 		knockback *= 0.92
 	else:
 		knockback = Vector2.ZERO
+		if projectile != null or agent.global_position.distance_squared_to(player.global_position) > sight_squared or bonk > 0:
+			var wander: Vector2 = Vector2(noise_x.get_noise_1d(noise_t), noise_y.get_noise_1d(noise_t)).normalized() * speed
+			noise_t += frequency
+			if not agent.move(wander):
+				reset_noise()
+			if bonk > 0:
+				bonk -= 1
+		else:
+			var direction: Vector2 = agent.global_position.direction_to(player.global_position) * speed
+			var could_x: bool = agent.move_x(direction.x, speed) and abs(direction.x) > speed / 8
+			var could_y: bool = agent.move_y(direction.y, speed) and abs(direction.y) > speed / 8
+			if not could_y and not could_x:
+				bonk = 45
 
 	if reload_time > 0:
 		reload_time -= 1
@@ -67,3 +91,12 @@ func _physics_process(_delta: float) -> void:
 				break
 			if agent.map.get_cell_tile_data(agent.map.local_to_map(agent.map.to_local(sample_pos))).get_custom_data("Solid"):
 				break
+
+func reset_noise():
+	noise_t = 0
+	noise_x.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	noise_y.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	noise_x.fractal_type = FastNoiseLite.FRACTAL_NONE
+	noise_y.fractal_type = FastNoiseLite.FRACTAL_NONE
+	noise_x.seed = randi()
+	noise_y.seed = randi()
